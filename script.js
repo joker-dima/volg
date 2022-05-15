6ada7e67a98c21698658749fc5312c6b8ed4cc5f
Дмитрий Малышев <joker-dima@yandex.ru>
2022-05-15 16:51:32 +0300

    JS burger menu is working

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 // Burger menu
 
-let navMain = document.querySelector(".main-nav");
-let navToggle = document.querySelector(".main-nav__toggle");
+let navMain = document.querySelector(".main-navigation");
+let navToggleOpen = document.querySelector(".main-navigation__togle--open");
+let navToggleClose = document.querySelector(".main-navigation--closed")
 
-
-navToggle.addEventListener('click', function () {
-    if (navMain.classList.contains('main-nav--closed')) {
-        navMain.classList.remove('main-nav--closed');
-        navMain.classList.add('main-nav--opened');
+navToggleOpen.addEventListener('click', function () {
+    if (navMain.classList.contains('main-navigation--closed')) {
+        navMain.classList.remove('main-navigation--closed');
+        navMain.classList.add('main-navigation');
     } else {
-        navMain.classList.add('main-nav--closed');
-        navMain.classList.remove('main-nav--opened');
+        navMain.classList.add('main-navigation--closed');
+        navMain.classList.remove('main-navigation');
     }
+});
 
+navToggleClose.addEventListener('click', function () {
+    if (navMain.classList.contains('main-navigation--closed')) {
+        navMain.classList.remove('main-navigation--closed');
+        navMain.classList.add('main-navigation');
+    } else {
+        navMain.classList.add('main-navigation--closed');
+        navMain.classList.remove('main-navigation');
+    }
 });
